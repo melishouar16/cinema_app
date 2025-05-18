b5c462a70379f87e6b3ddc8dc5b6d84fb77ab544
melissa <meliouar16@gmail.com>
2025-05-18 13:32:34 +0200

    add api

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuteurViewSet, FilmViewSet
+
+router = DefaultRouter()
+router.register(r'auteurs', AuteurViewSet)
+router.register(r'films', FilmViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
---
+++ movies/views.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
+from .models import Auteur, Film
+from .serializers import AuteurSerializer, FilmSerializer
+
+
+class AuteurViewSet(viewsets.ModelViewSet):
+    queryset = Auteur.objects.all()
+    serializer_class = AuteurSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        auteur = self.get_object()
+        if auteur.films.count() > 0:
+            return Response(
+                {"erreur": "Impossible de supprimer cet auteur car il a des films associés"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().destroy(request, *args, **kwargs)
+
+
+class FilmViewSet(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+    def get_queryset(self):
+        queryset = Film.objects.all()
+        statut = self.request.query_params.get('statut')
+        if statut:
+            queryset = queryset.filter(statut=statut)
+        return queryset
+
+    @action(detail=True, methods=['patch'])
+    def archiver(self, request, pk=None):
+        film = self.get_object()
+        film.statut = 'archive'
+        film.save()
+        return Response({'statut': 'Film archivé'})
