66826ab55e2c8f97afe941bf9f520187ebe502d6
melissa <meliouar16@gmail.com>
2025-06-29 15:17:41 +0200

    implémenter PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,3 +15,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// PWA Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('SW enregistré: ', registration);
+      })
+      .catch((error) => {
+        console.log('Erreur SW: ', error);
+      });
+  });
+}
